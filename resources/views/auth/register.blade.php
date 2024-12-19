bef0f1c587784b6848f544dc5a2b344de8aa9827
sessi123 <zinmonsesylvie@gmail.com>
2024-12-19 09:28:28 +0100

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,17 +1,16 @@
 @extends('auth.template')
+
 @section('content')
     <h2 class="lh-base mb-0">Inscription </h2>
     <p>
-        Regnoignez-nous en quelques clics pour profiter de nos services.
+        Rejoignez-nous en quelques clics pour profiter de nos services.
     </p>
     <hr>
     <div class="row">
-
         <div class="col-12">
             <a class="btn btn-white shadow-sm text-dark link-primary border fw-semibold d-flex align-items-center justify-content-center rounded-1 py-6"
                 href="{{ url('/login/google') }}" role="button">
-                <img src="{{ asset('matdash/google-icon.svg') }}" alt="matdash-img" class="img-fluid me-2" width="18"
-                    height="18">
+                <img src="{{ asset('matdash/google-icon.svg') }}" alt="matdash-img" class="img-fluid me-2" width="18" height="18">
                 <span class="d-none d-xxl-inline-flex"> S'inscrire avec Google</span>
             </a>
         </div>
@@ -25,9 +24,9 @@
     <form method="POST" action="{{ route('register') }}" role="form">
         @csrf
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Nom complet <strong class="text-danger">*</strong> </label>
-            <input id="name" type="text" class="form-control  @error('name') is-invalid @enderror" required
-                autocomplete="name" autofocus name="name" value="{{ old('name') }}">
+            <label for="name" class="form-label">Nom complet <strong class="text-danger">*</strong></label>
+            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                value="{{ old('name') }}" required autocomplete="name" autofocus>
             @error('name')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -36,9 +35,9 @@
         </div>
 
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email / Identifiant<strong class="text-danger">*</strong></label>
-            <input id="email" type="email" class="form-control  @error('email') is-invalid @enderror"
-                id="exampleInputEmail1" required autocomplete="email" autofocus name="email" value="{{ old('email') }}">
+            <label for="email" class="form-label">Email / Identifiant</label>
+            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                value="{{ old('email') }}" required autocomplete="email">
             @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -46,16 +45,10 @@
             @enderror
         </div>
 
-
         <div class="mb-4">
-            <div class="d-flex align-items-center justify-content-between">
-                <label for="exampleInputPassword1" class="form-label">Mot de passe<strong class="text-danger">*</strong></label>
-            </div>
-            <div class="pass_show_hide">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-                    name="password" required autocomplete="password">
-            </div>
 
+            <label for="password" class="form-label">Mot de passe</label>
+            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
             @error('password')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -63,81 +56,18 @@
             @enderror
         </div>
 
-        <div class="mb-3">
-            <label for="telephone" class="form-label">Numéro de téléphone <strong class="text-danger"></strong> </label>
-            <input id="telephone" type="tel" class="form-control  @error('telephone') is-invalid @enderror"
-                autocomplete="telephone" autofocus name="Telephone" value="{{ old('telephone') }}">
-            @error('telephone')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
-
-
         <button type="submit" class="btn btn-dark w-100 py-8 mb-4 rounded-1">S'inscrire</button>
 
-
         <div class="d-flex align-items-center">
-            <p class="fs-12 mb-0 fw-medium">Avez-voud déjà de compte chez nous ?</p>
+            <p class="fs-12 mb-0 fw-medium">Avez-vous déjà un compte chez nous ?</p>
             <a class="text-success fw-bolder ms-2" href="{{ route('login') }}">Se connecter</a>
         </div>
     </form>
 @endsection
 
 @section('illustrations')
-    <img src="{{ asset('matdash/tr2.png') }}" class="w-100 img-fluid my-5" alt="...">
+    <img src="{{ asset('matdash/tr2.png') }}" class="w-100 img-fluid my-5" alt="Illustration d'inscription">
     <p class="text-center">
-        Veuillez vous inscrire pour accéder à votre espace personnel et profiter de nos services
+        Veuillez vous inscrire pour accéder à votre espace personnel et profiter de nos services.
     </p>
-    {{-- <div id="auth-login" class="carousel slide auth-carousel mt-5 pt-4" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#auth-login" data-bs-slide-to="0" class="active" aria-current="true"
-                aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#auth-login" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#auth-login" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="d-flex align-items-center justify-content-center w-100 h-100 flex-column gap-9 text-center">
-                    <img src="../assets/images/backgrounds/login-side.png" alt="login-side-img" width="300"
-                        class="img-fluid" />
-                    <h4 class="mb-0">Feature Rich 3D Charts</h4>
-                    <p class="fs-12 mb-0">Donec justo tortor, malesuada
-                        vitae
-                        faucibus ac, tristique sit amet
-                        massa.
-                        Aliquam dignissim nec felis quis imperdiet.</p>
-                    <a href="javascript:void(0)" class="btn btn-primary rounded-1">Learn More</a>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <div class="d-flex align-items-center justify-content-center w-100 h-100 flex-column gap-9 text-center">
-                    <img src="../assets/images/backgrounds/login-side.png" alt="login-side-img" width="300"
-                        class="img-fluid" />
-                    <h4 class="mb-0">Feature Rich 2D Charts</h4>
-                    <p class="fs-12 mb-0">Donec justo tortor, malesuada
-                        vitae
-                        faucibus ac, tristique sit amet
-                        massa.
-                        Aliquam dignissim nec felis quis imperdiet.</p>
-                    <a href="javascript:void(0)" class="btn btn-primary rounded-1">Learn More</a>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <div class="d-flex align-items-center justify-content-center w-100 h-100 flex-column gap-9 text-center">
-                    <img src="../assets/images/backgrounds/login-side.png" alt="login-side-img" width="300"
-                        class="img-fluid" />
-                    <h4 class="mb-0">Feature Rich 1D Charts</h4>
-                    <p class="fs-12 mb-0">Donec justo tortor, malesuada
-                        vitae
-                        faucibus ac, tristique sit amet
-                        massa.
-                        Aliquam dignissim nec felis quis imperdiet.</p>
-                    <a href="javascript:void(0)" class="btn btn-primary rounded-1">Learn More</a>
-                </div>
-            </div>
-        </div>
-
-    </div> --}}
 @endsection
--- resources/views/auth/show-hide-password.blade.php
+++ resources/views/auth/show-hide-password.blade.php
@@ -1,104 +1,11 @@
-{{-- <style>
-    .label-pw {
-        position: relative;
-    }
-
-    .label-pw
-    .password-icon {
-        display: flex;
-        align-items: center;
-        position: absolute;
-        top: 50%;
-        right: 20px;
-        transform: translateY(-50%);
-        width: 20px;
-        /* color: #f9f9f9; */
-        transition: all 0.2s;
-    }
-
-    .label-pw .password-icon:hover {
-        cursor: pointer;
-        color: #ff4754;
-    }
-
-    .label-pw .password-icon .feather-eye-off {
-        display: none;
-    }
-</style>
-<script src="https://unpkg.com/feather-icons"></script>
-
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
 <script>
     $(document).ready(function() {
-        console.log("échargement fini")
-    });
-    feather.replace();
 
-    const eye = document.querySelectorAll(".feather-eye");
-    const eyeoff = document.querySelectorAll(".feather-eye-off");
-    const passwordField = document.querySelectorAll("input[type=password]");
-
-    eye.forEach(element => {
-        element.addEventListener("click", (e) => {
-            element.style.display = "none";
-            let label = element.closest(".label-pw");
-            label.querySelector(".feather-eye-off").style.display = "block";
-            let label2 = element.closest(".label-pw");
-            label2.querySelector("input[type=password]").type = "text";
-
-            // console.log(p);
-            // var p = element.closest(".feather-eye-off");
-            // var p = element.parentNode.querySelector('.feather-eye-off');
-            // p.style.display = "block";
-            // var pass = element.parentNode.parentNode.querySelector('input[type=password]');
-            // pass.type = "text";
-        });
-    });
-
-    eyeoff.forEach(element => {
-        element.addEventListener("click", () => {
-            element.style.display = "none";
-            let label = element.closest(".label-pw");
-            let ee = label.querySelector(".feather-eye");
-            ee.style.display = "block";
-
-            let inp = element.closest(".label-pw").querySelector("input[type=password]");
-            console.log(inp);
-            inp.type = "text";
-
-
-            // var p = element.parentNode.querySelector('.feather-eye');
-            // p.style.display = "block";
-            // var pass = element.parentNode.parentNode.querySelector('input[type=password]');
-            // console.log(pass);
-            // pass.type = "password";
-
-            // eyeoff.style.display = "none";
-            // eye.style.display = "block";
-            // passwordField.type = "password";
-        });
-    });
-
-    // const eye = document.querySelector(".feather-eye");
-    // const eyeoff = document.querySelector(".feather-eye-off");
-    // const passwordField = document.querySelector("input[type=password]");
-
-    // eye.addEventListener("click", () => {
-    //     eye.style.display = "none";
-    //     eyeoff.style.display = "block";
-    //     passwordField.type = "text";
-    // });
-
-    // eyeoff.addEventListener("click", () => {
-    //     eyeoff.style.display = "none";
-    //     eye.style.display = "block";
-    //     passwordField.type = "password";
-    // });
-</script> --}}
-
-<script>
-    $(document).ready(function() {
         $('.pass_show_hide').append(
             ' <div class="password-icon"><i class="fa fa-eye"></i><i class="fa fa-eye-slash"></i></div>');
+
     });
 
 
